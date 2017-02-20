368308d503bca0e109d7485ac363c01d0a1528fb
Slava.Rybalka <slava.rybalka@Administrators-MacBook-Pro-5.local>
2017-02-20 14:18:22 -0600

    update: added database
    Added database.

---
+++ ll_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/slava.rybalka/Documents/Pyscripts/Django_projects/ll_env/bin/python3.5
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
