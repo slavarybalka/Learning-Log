cb1cc0775366190451861436d7ba5914de2e6ce3
Slava.Rybalka <slavarybalko@gmail.com>
2017-03-06 15:15:36 -0600

    update: Added forms.py
    Added forms.py, page 428

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -7,4 +7,11 @@ from . import views
 urlpatterns = [
     # Home page
     url(r'^$', views.index, name='index'),
+
+    # Show all topics.
+    url(r'^topics/$', views.topics, name="topics"),
+
+    # Detail page for a single topic
+    url(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
+
 ]
